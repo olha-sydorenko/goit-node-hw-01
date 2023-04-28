61b1cf1422c5bff29dd95d7dc7bc051a40fd8b50
olha-sydorenko <108460682+olha-sydorenko@users.noreply.github.com>
2023-04-28 16:08:34 +0300

    CLI

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # goit-node-hw-01
+
+Отримуємо і виводимо весь список контактів у вигляді таблиці (console.table)
+https://prnt.sc/hVXCxfllHWTU
+
+Отримуємо контакт по id
+https://prnt.sc/FaT144p1OvbJ
+
+Додаємо контакт
+https://prnt.sc/lcR24ceZ2yjm
+
+Видаляємо контакт
+https://prnt.sc/hHSP-Y4hrNjw
+
+
--- index.js
+++ index.js
@@ -1,20 +1,37 @@
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+
 const contacts = require("./contacts");
 
-const invokeAction = async ({action, contactId, name, email, phone}) => {
+async function invokeAction ({action, id, name, email, phone}) {
     switch(action) {
         case "list":
             const allContacts = await contacts.listContacts();
-            return console.log(allContacts);
+            return console.table(allContacts);
         case "get":
-            const oneContact = await contacts.getContactById(contactId);
+            const oneContact = await contacts.getContactById(id);
             return console.log(oneContact);   
         case "add":
             const newContact = await contacts.addContact({name, email, phone});
             return console.log(newContact);
         case "remove":
-            const removedContact = await contacts.removeContact(contactId);
+            const removedContact = await contacts.removeContact(id);
             return console.log(removedContact);
+        default:
+                console.warn("\x1B[31m Unknown action type!");    
 
     }
 }
-invokeAction({action: "remove", contactId: "fL4OH8i1PEPCLNTFA6vPo"});
+invokeAction(argv);
+//invokeAction ({action: "get", id: "Z5sbDlS7pCzNsnAHLtDJd"});
